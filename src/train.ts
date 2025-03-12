5889f8bc4fb00cc263b81eb5f7bd3f9391cf3745
amurkhon <akramjonovamurxon02@gmail.com>
2025-03-13 00:20:27 +0900

    feat: express integrations

--- src/train.ts
+++ src/train.ts
@@ -1,11 +1,23 @@
 // G-TASK
 
-function  getHighestIndex(array: number[]) {
-    const maxValue = Math.max.apply(null,array);
-    const index = array.findIndex((value, index) => {
-        return value == maxValue;
-});
-return index;
+// function  getHighestIndex(array: number[]) {
+//     const maxValue = Math.max.apply(null,array);
+//     const index = array.findIndex((value, index) => {
+//         return value == maxValue;
+// });
+// return index;
+// }
+// const result = getHighestIndex([23,32,12,45,45,65]);
+// console.log(result);
+
+function getPositive(list: number[]){
+    let positive = '';
+    for(let i of list) {
+        if( i > 0) {
+            positive += i;
+        }
+    }
+    return positive;
 }
-const result = getHighestIndex([23,32,12,45,45,65]);
-console.log(result);
+const positive_numbers = getPositive([1,2,3,-4,-5,6,7]);
+console.log(positive_numbers);
