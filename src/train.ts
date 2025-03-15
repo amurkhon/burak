d077fcbfb0a5e0bd6e822d3132b2f4f7fd1bed71
amurkhon <akramjonovamurxon02@gmail.com>
2025-03-15 14:56:54 +0900

    feat: develop member schema model

--- src/train.ts
+++ src/train.ts
@@ -1,5 +1,7 @@
 // G-TASK
 
+import { count } from "console";
+
 // function  getHighestIndex(array: number[]) {
 //     const maxValue = Math.max.apply(null,array);
 //     const index = array.findIndex((value, index) => {
@@ -10,14 +12,32 @@
 // const result = getHighestIndex([23,32,12,45,45,65]);
 // console.log(result);
 
-function getPositive(list: number[]){
-    let positive = '';
-    for(let i of list) {
-        if( i > 0) {
-            positive += i;
-        }
+// function getPositive(list: number[]){
+//     let positive = '';
+//     for(let i of list) {
+//         if( i > 0) {
+//             positive += i;
+//         }
+//     }
+//     return positive;
+// }
+// const positive_numbers = getPositive([1,2,3,-4,-5,6,7]);
+// console.log(positive_numbers);
+
+// I-TASK
+
+function majorityElement(array: number[]) {
+    let max = 0;
+    let number;
+    for( let i=1; i<=9; i++) {
+       let count = array.filter(x => x==i).length;
+       if( count > max){
+        max = count;
+        number = i;
+       }
     }
-    return positive;
+    return number;
 }
-const positive_numbers = getPositive([1,2,3,-4,-5,6,7]);
-console.log(positive_numbers);
+
+const result = majorityElement([1,2,2,2,3,3,3,3]);
+console.log(result);
