a5f0d612dbc70420a7ad3c328394fa66113f321d
amurkhon <akramjonovamurxon02@gmail.com>
2025-11-09 16:41:36 +0900

    feat: develop Socket.io Server

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import morgan from 'morgan';
 import { MORGAN_FORMAT } from './libs/config';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import { Server as SocketIOServer } from 'socket.io';
+import http from 'http';
 
 import session from "express-session";
 import ConnectMongoDB from "connect-mongodb-session";
@@ -64,4 +66,23 @@ app.set('view engine', 'ejs');
 app.use('/admin', routerAdmin); // EJS
 app.use('/', router);  //REACT
 
-export default app;
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+});
+
+let summaryClient = 0;
+io.on("connection", (socket) => {
+  summaryClient++;
+  console.log(`Connection & total [${summaryClient}]`);
+
+  socket.on('disconnect', () => {
+    summaryClient--;
+    console.log(`Disconnection & total [${summaryClient}]`);
+  });
+});
+
+export default server;
